13ef321ad9c69ff937b4c0d254745848402e8c7a
LailaWulandarii <E41220418@student.polije.ac.id>
2025-08-24 15:00:27 +0700

    rename custom bouquet's migration file

--- database/migrations/2025_08_23_050724_create_custom_bouquet_table.php
+++ database/migrations/2025_08_23_050724_create_custom_bouquet_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('buket_kustom', function (Blueprint $table) {
+        Schema::create('custom_bouquet', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('buket_kustom');
+        Schema::dropIfExists('custom_bouquet');
     }
 };
