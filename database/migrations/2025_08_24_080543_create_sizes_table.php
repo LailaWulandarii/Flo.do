3bf5bf47f6af070eabe8200f174653fb8b100257
LailaWulandarii <E41220418@student.polije.ac.id>
2025-08-24 15:08:48 +0700

    rename size's table

--- database/migrations/2025_08_24_080543_create_sizes_table.php
+++ database/migrations/2025_08_24_080543_create_sizes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('size', function (Blueprint $table) {
+        Schema::create('sizes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('size');
+        Schema::dropIfExists('sizes');
     }
 };
