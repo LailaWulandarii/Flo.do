25d84ca8f7cac4ecbc4e7fe42fc27c4ac5be61cf
LailaWulandarii <E41220418@student.polije.ac.id>
2025-08-24 15:01:37 +0700

    rename bouquet migration's file

--- database/migrations/2025_08_23_050437_create_bouquet_table.php
+++ database/migrations/2025_08_23_050437_create_bouquet_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('buket', function (Blueprint $table) {
+        Schema::create('bouquet', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->text('deskripsi')->nullable();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('buket');
+        Schema::dropIfExists('bouquet');
     }
 };
